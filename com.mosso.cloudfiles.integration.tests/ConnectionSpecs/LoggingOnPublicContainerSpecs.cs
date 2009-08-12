32ffff919af8f999e8424e7ef953f686dd140e0c
unknown <rssvihla@gmail.com>
2009-08-12 09:59:57 -0500

    completing tag

---
+++ com.mosso.cloudfiles.integration.tests/ConnectionSpecs/LoggingOnPublicContainerSpecs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace com.mosso.cloudfiles.integration.tests.ConnectionSpecs
+{
+    [TestFixture]
+    public class LoggingOnPublicContainerSpecs : TestBase
+    {
+        [Test]
+        public void should_add_logging_to_public_container()
+        {
+            try
+            {
+                connection.CreateContainer(Constants.CONTAINER_NAME);
+                connection.SetLoggingOnPublicContainer(Constants.CONTAINER_NAME, true);
+                var container = connection.GetContainers();
+            }
+            catch (Exception ex)
+            {
+                var ex1 = ex;
+            }
+            finally
+            {
+
+                connection.DeleteContainer(Constants.CONTAINER_NAME);
+            }
+
+        }
+    }
+}
